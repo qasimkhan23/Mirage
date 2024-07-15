2d940fcf661c63da04fad8d320fc3868071a9b5b
hashir2024 <hasirwa@gmail.com>
2024-07-15 16:53:22 +0500

    fix

---
+++ src/screens/Pack/pack.jsx
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from "react";
+import {
+   Image,
+   Text,
+   TextInput,
+   TouchableOpacity,
+   View,
+   ScrollView,
+   ImageBackground,
+} from "react-native";
+import {
+   Background,
+   rightCardP,
+   rightCardPr,
+   vpp,
+   vpP,
+   mpP,
+   mpp,
+   MonthPass,
+   VIPpass,
+   leftCardP,
+   leftCardPr,
+   LogoLeft,
+   LogoRight,
+   BackBtn,
+   Top50,
+   Rect,
+   Track1,
+   Track2,
+   trendF,
+   One,
+   Second,
+   Third,
+   AwardLogo,
+   ThreeDays,
+   PinkRect,
+   MusicLogo,
+   AwardOne,
+   AwardTwo,
+   PackBackground,
+   PopSec,
+   SubSec,
+   CardSec,
+   CardOne,
+   CardPa,
+} from "../../../assets";
+
+import styles from "./styles";
+
+const PackScreen = ({ navigation }) => {
+   return (
+      <ImageBackground source={ShopBackground} style={[styles.container]}>
+         <View style={styles.topConta}>
+            <View style={styles.logoCont}>
+               <Image source={LogoLeft} style={styles.logo} />
+               <Text style={styles.logoText}>5400</Text>
+            </View>
+
+            <View style={styles.logoCont}>
+               <Image source={LogoRight} style={styles.logo} />
+               <Text style={styles.logoText}>234</Text>
+            </View>
+         </View>
+         <View style={styles.pagelogo}>
+            <TouchableOpacity style={styles.backBtn}>
+               <Image source={BackBtn} style={styles.Btn} />
+            </TouchableOpacity>
+
+            <View style={styles.pageTop}>
+               <Text style={styles.topText}>mirage shop</Text>
+            </View>
+         </View>
+         <View style={styles.bigContainer}>
+            <ScrollView style={styles.verticalScrollView}>
+               <View style={styles.popsec}>
+                  <View style={styles.popCont}>
+                     <Image source={PopSec} style={styles.popimg} />
+                  </View>
+                  <View style={styles.popMid}>
+                     <Text style={styles.deal1}>10 Mirage Gem {"\n"} Pack</Text>
+                     <Text style={styles.deal2}>100 Mirage Gem {"\n"}Pack</Text>
+                     <Text style={styles.deal3}>
+                        1000 Mirage {"\n"} Gem Pack
+                     </Text>
+                  </View>
+               </View>
+
+               <View style={styles.cardAndPack}>
+                  <Text style={styles.cardAndPackH}> Cards & Gems Pack</Text>
+               </View>
+
+               <View style={styles.cardAndPackCont}>
+                  <View style={styles.cardAndPackPics}>
+                     <View style={styles.cardContainer}>
+                        <Image
+                           style={styles.cardAndPackImageL}
+                           source={CardOne}
+                        />
+                        <Text style={styles.TextL}>
+                           100 Mirage Gems & 10 Card Packs
+                        </Text>
+                        <Image style={styles.leftPrice} source={leftCardPr} />
+                        <Image style={styles.leftPrice1} source={leftCardP} />
+                        <TouchableOpacity style={styles.buyBox}>
+                           <Text style={styles.buyText}> BUY NOW</Text>
+                        </TouchableOpacity>
+                     </View>
+                     <View style={styles.middleLine} />
+                     <View style={styles.cardContainer}>
+                        <Image
+                           style={styles.cardAndPackImageR}
+                           source={CardOne}
+                        />
+                        <Text style={styles.TextR}>1 Mirage Card Pack </Text>
+                        <Image style={styles.rightPrice} source={rightCardPr} />
+                        <Image style={styles.rightPrice1} source={rightCardP} />
+
+                        <View style={styles.buyBoxRight}>
+                           <TouchableOpacity style={styles.buyBoxM}>
+                              <Text style={styles.buyTextM}> - </Text>
+                           </TouchableOpacity>
+
+                           <TouchableOpacity style={styles.buyBoxR}>
+                              <Text style={styles.buyTextR}> BUY NOW</Text>
+                           </TouchableOpacity>
+
+                           <TouchableOpacity style={styles.buyBoxP}>
+                              <Text style={styles.buyTextP}> + </Text>
+                           </TouchableOpacity>
+                        </View>
+                     </View>
+                  </View>
+               </View>
+
+               <View style={styles.cardAndPack}>
+                  <Text style={styles.cardAndPackH}> Subscriptions</Text>
+               </View>
+               {/* <View style={styles.subsec}>
+            <View style={styles.subCont}>
+            <Image source={SubSec} style={styles.Subimg}/>
+            </View>
+            <View style={styles.subMid}>
+               <Text style={styles.subDeal1}>Mirage Monthly</Text>
+               <Text style={styles.subDeal2}>Mirage VIP Pass</Text>
+               </View>
+            </View> */}
+
+               <View style={styles.PassPics}>
+                  <View style={styles.passTextContainer}>
+                     <Image style={styles.passPicL} source={MonthPass} />
+                     <View>
+                        <Text style={styles.PassTextLeft}>
+                           Mirage Monthly Pass
+                        </Text>
+                        <TouchableOpacity>
+                           <Image style={styles.leftPassPrice1} source={mpP} />
+                        </TouchableOpacity>
+                     </View>
+                  </View>
+                  <View style={styles.passTextContainer}>
+                     <Image style={styles.passPicR} source={VIPpass} />
+                     <View>
+                        <Text style={styles.PassTextRight}>
+                           Mirage VIP Pass{" "}
+                        </Text>
+                        <TouchableOpacity>
+                           <Image style={styles.rightPassPrice} source={vpp} />
+                        </TouchableOpacity>
+                     </View>
+                  </View>
+               </View>
+            </ScrollView>
+         </View>
+      </ImageBackground>
+   );
+};
+
+export default PackScreen;
---
+++ src/screens/Pack/styles.js
@@ -0,0 +1,386 @@
+import { StyleSheet } from "react-native";
+import { BackBtn } from "../../../assets";
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      justifyContent: "flex-start",
+      alignItems: "center",
+      width: "100%",
+      height: "100%",
+      resizeMode: "cover",
+   },
+   bigContainer: {
+      display: "flex",
+      flex: 1,
+      // alignItems: 'center',
+      marginHorizontal: 5,
+      fontFamily: "regular",
+      // height:'100%'
+   },
+   verticalScrollView: {
+      // width: '100%',
+      display: "flex",
+      flex: 1,
+      paddingHorizontal: 10,
+   },
+
+   topConta: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      width: "100%",
+      paddingHorizontal: 20,
+      marginTop: 35,
+   },
+
+   logoCont: {
+      flexDirection: "row",
+      alignItems: "center",
+   },
+
+   logo: {
+      width: 20,
+      height: 20,
+      marginRight: 12,
+   },
+
+   logoText: {
+      color: "#fff",
+      fontSize: 12.1,
+      fontFamily: "regular",
+   },
+   pagelogo: {
+      flexDirection: "row",
+      alignItems: "center",
+      width: "100%",
+      justifyContent: "space-between",
+   },
+   pageTop: {
+      flex: 0.7,
+      textAlign: "center",
+      justifyContent: "center",
+      marginTop: 12,
+   },
+   topText: {
+      color: "#fff",
+      fontSize: 30,
+      fontFamily: "regular",
+   },
+   backBtn: {
+      // display:'flex',
+      // flex:1,
+      justifyContent: "flex-start",
+   },
+   Btn: {
+      width: 35,
+      height: 20,
+      resizeMode: "contain",
+      marginLeft: -5,
+   },
+
+   text: {
+      color: "#fff",
+      fontSize: "18",
+      fontFamily: "regular",
+   },
+
+   popsec: {
+      justifyContent: "center",
+      alignItems: "center",
+      paddingVertical: 10,
+   },
+   popCont: {
+      // width: "100%",
+   },
+   popimg: {
+      width: 370,
+      height: 190,
+      resizeMode: "contain",
+   },
+   popMid:{
+      flexDirection:'row',
+      justifyContent:'space-between',
+      alignItems:'center',
+      width:'70%',
+      position:'absolute',
+      top:'84%',
+      left:'20%',
+      // width:'95%',
+      // paddingLeft:18,
+   },
+    
+   deal1:{
+      color: '#fff',
+      fontSize: 8,
+      fontFamily: 'regular',
+   },
+   deal2:{
+      color: '#fff',
+      fontSize: 8,
+      fontFamily: 'regular',
+      // paddingLeft:25,
+   },
+   deal3:{
+      color: '#fff',
+      fontSize: 8,
+      fontFamily: 'regular',
+      marginRight:-5,
+   },
+
+   deal1: {
+      color: "#fff",
+      fontSize: 8,
+      fontFamily: "regular",
+   },
+   deal2: {
+      color: "#fff",
+      fontSize: 8,
+      fontFamily: "regular",
+   },
+   deal3: {
+      color: "#fff",
+      fontSize: 8,
+      fontFamily: "regular",
+   },
+
+   cardAndPack: {
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 15,
+   },
+   cardAndPackH: {
+      color: "#fff",
+      fontSize: 22,
+      fontFamily: "regular",
+   },
+
+   cardAndPackPics: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      // paddingHorizontal:-20,
+   },
+
+   cardAndPackCont: {
+      borderWidth: 0.2, // Adjust the thickness as needed
+      borderRadius: 4, // Adjust the radius as needed
+      borderColor: "white", // White border color
+      backgroundColor: "rgba(169, 169, 169,0.1)",
+      marginTop: 30,
+   },
+   cardContainer: {
+      alignItems: "center",
+      marginBottom: 10,
+   },
+   cardAndPackImageR: {
+      height: 130,
+      width: 80,
+      top: -30,
+   },
+   cardAndPackImageL: {
+      height: 130,
+      width: 80,
+      top: -30,
+   },
+
+   cardAndPackText: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+   },
+   TextL: {
+      color: "#fff",
+      fontSize: 12,
+      fontFamily: "regular",
+      marginTop: -35,
+   },
+   TextR: {
+      color: "#fff",
+      fontSize: 12,
+      fontFamily: "regular",
+      marginTop: -35,
+   },
+   leftPrice: {
+      height: 30,
+      width: 40,
+   },
+   leftPrice1: {
+      height: 30,
+      width: 40,
+      marginTop: -30,
+   },
+   rightPrice: {
+      height: 30,
+      width: 40,
+   },
+   rightPrice1: {
+      height: 30,
+      width: 40,
+      marginTop: -30,
+   },
+
+   buyBox: {
+      width: 120,
+      height: 25,
+      borderWidth: 0.2,
+      borderRadius: 2,
+      borderColor: "white",
+      backgroundColor: "rgba(169, 169, 169,0.1)",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   buyText: {
+      color: "#fff",
+      fontSize: 10,
+      fontFamily: "Arial",
+   },
+
+   buyBoxRight: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   buyBoxM: {
+      width: 30,
+      height: 25,
+      borderWidth: 0.2,
+      borderRadius: 2,
+      borderColor: "white",
+      backgroundColor: "rgba(169, 169, 169,0.1)",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   buyTextM: {
+      color: "#fff",
+      fontSize: 16,
+      fontFamily: "Arial",
+   },
+   buyBoxR: {
+      width: 70,
+      height: 25,
+      borderWidth: 0.2,
+      borderRadius: 2,
+      borderColor: "white",
+      backgroundColor: "rgba(169, 169, 169,0.1)",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      marginRight: 5,
+      marginLeft: 5,
+   },
+   buyTextR: {
+      color: "#fff",
+      fontSize: 10,
+      fontFamily: "Arial",
+   },
+
+   buyBoxP: {
+      width: 30,
+      height: 25,
+      borderWidth: 0.2,
+      borderRadius: 2,
+      borderColor: "white",
+      backgroundColor: "rgba(169, 169, 169,0.1)",
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+   },
+   buyTextP: {
+      color: "#fff",
+      fontSize: 14,
+      fontFamily: "Arial",
+   },
+
+   subsec: {
+      justifyContent: "center",
+      alignItems: "center",
+      paddingVertical: -10,
+   },
+   subCont: {
+      width: "100%",
+      height: 80,
+      justifyContent: "center",
+      alignItems: "center",
+   },
+   Subimg: {
+      width: "100%",
+      height: "100%",
+      resizeMode: "contain",
+   },
+
+   subMid: {
+      // position:'absolute',
+      flexDirection: "row",
+      justifyContent: "space-around",
+      alignItems: "center",
+      width: "70%",
+      // marginTop:-100,
+   },
+   subDeal1: {
+      color: "#fff",
+      fontSize: 14,
+      fontFamily: "regular",
+      marginTop: -15,
+   },
+   subDeal2: {
+      color: "#fff",
+      fontSize: 14,
+      fontFamily: "regular",
+      marginRight: -40,
+   },
+
+   PassPics: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+      alignItems: "center",
+      width: "100%",
+      paddingHorizontal: -35,
+   },
+   passTextContainer: {
+      alignItems: "center",
+      marginBottom: 10,
+      flexDirection: "row",
+   },
+   passPicR: {
+      height: 80,
+      width: 90,
+      marginLeft: 15,
+   },
+   passPicL: {
+      height: 80,
+      width: 90,
+   },
+
+   cardAndPackText: {
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+   },
+   PassTextLeft: {
+      color: "#fff",
+      fontSize: 12,
+      fontFamily: "regular",
+   },
+   PassTextRight: {
+      color: "#fff",
+      fontSize: 12,
+      fontFamily: "regular",
+   },
+   leftPassPrice1: {
+      height: 25,
+      width: 40,
+      marginLeft: -5,
+   },
+   rightPassPrice: {
+      height: 25,
+      width: 70,
+      marginLeft: -10,
+   },
+});
+export default styles;
